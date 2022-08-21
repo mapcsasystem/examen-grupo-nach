c393cfae24f00e0ef2b58e9f7de49ea6e800c1b9
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-08-21 16:00:18 -0500

    first commit

---
+++ src/test/java/com/examen/nach/NachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.nach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
